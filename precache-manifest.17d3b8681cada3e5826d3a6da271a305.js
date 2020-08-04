da7438fd7d576f1755a8a0004b764284578b1613
Vivek Raghuvanshi <vivekraghuv8@gmail.com>
2020-08-04 22:32:56 +0530

    Updates

---
+++ precache-manifest.17d3b8681cada3e5826d3a6da271a305.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8a3b065ffb1a53553312bb9c4c1c1ed",
+    "url": "/notePad/index.html"
+  },
+  {
+    "revision": "8e56c4ca178e758b1f86",
+    "url": "/notePad/static/css/main.f119bd74.chunk.css"
+  },
+  {
+    "revision": "40c012efb164d561fc46",
+    "url": "/notePad/static/js/2.49b5f06e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/notePad/static/js/2.49b5f06e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e56c4ca178e758b1f86",
+    "url": "/notePad/static/js/main.dc076db7.chunk.js"
+  },
+  {
+    "revision": "d86bde655e9834a10322",
+    "url": "/notePad/static/js/runtime-main.7425b870.js"
+  }
+]);
